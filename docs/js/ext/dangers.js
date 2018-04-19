6a0e69b84f71f5eface21ecf65e073ddf994e9b2
Andrew Gremlich <andrew.gremlich@gm.com>
2018-04-19 16:36:01 -0500

    Workable web interface!
    rename folder for github pages deployment.
    
    No automated blinking caret for terminal like interface.  Want to make
    it thicker though.
    
    Clear input on successful command.
    
    took out dangers into another file.  more dangers?
    
    Single line emojis with sentences outputs.

---
+++ docs/js/ext/dangers.js
@@ -0,0 +1,12 @@
+const dangers = [
+  'You fell off a cliff. Yahoooooooooo!',
+  'A pack of wolves got you.  You are not Mowgli?',
+  'A blizzard trapped you.  Not from Dairy Queen.',
+  'A bear mauled you.  No safety there!',
+  'You saw bigfoot???',
+  'A penguin speared you with its beak.',
+  'A moose licked and left you in its tracks.',
+  'A reindeer ran over you.'
+]
+
+export default dangers
